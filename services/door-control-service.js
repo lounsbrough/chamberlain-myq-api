2a39cb6b2dca7497f295c46ddb4b8e847b672f63
David Lounsbrough <david.lounsbrough@gmail.com>
2018-10-24 20:25:03 -0500

    Extract logging service

--- services/door-control-service.js
+++ services/door-control-service.js
@@ -1,31 +1,7 @@
 const request = require('async-request')
-const winston = require('winston')
-const fs = require('fs')
 const sleep = require('system-sleep')
 
-const logDir = 'log'
-
-if (!fs.existsSync(logDir)) {
-    fs.mkdirSync(logDir)
-}
-const tsFormat = () => (new Date()).toLocaleString()
-const logger = new (winston.Logger)({
-    transports: [
-        new (winston.transports.Console)({
-            timestamp: tsFormat,
-            colorize: true,
-            level: 'verbose'
-        }),
-        new (require('winston-daily-rotate-file'))({
-            filename: `${logDir}/-results.log`,
-            timestamp: tsFormat,
-            datePattern: 'yyyy-MM-dd',
-            prepend: true,
-            level: 'verbose',
-            maxDays: 30
-        })
-    ]
-})
+const logger = require('./logging-service')
 
 let SecurityToken
 
